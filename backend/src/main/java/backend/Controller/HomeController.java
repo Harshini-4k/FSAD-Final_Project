635ae5da173814a07c4404864f5fe8ae18cb258b
Harshini <harshinir785@gmail.com>
2026-03-24 15:00:19 +0530

    Add Certification controller skeleton

---
+++ backend/src/main/java/backend/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "Backend is running successfully!";
+    }
+}
